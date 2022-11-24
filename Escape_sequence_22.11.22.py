600301ccf1a76f98a450f2d2979310ab0d276838
TymoshenkoOl <118847802+TymoshenkoOl@users.noreply.github.com>
2022-11-24 23:33:07 +0200

    Some useful combinations - "escape sequences"

---
+++ Escape_sequence_22.11.22.py
@@ -0,0 +1,8 @@
+escape_sequences = ('\\a - Bell (alert) \n'
+                    '\\b - Backspace \n'
+                    '\\n - New line \n'
+                    '\\t - Horizontal tab \n'
+                    '\\\\ - Backslash \\ \n'
+                    '\\" - Double quotation mark \" \n'
+                    "\\' - Single quotation mark \'")
+print(escape_sequences)
