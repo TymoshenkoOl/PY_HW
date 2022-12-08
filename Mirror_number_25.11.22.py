ec70352af032d6860cb840e25118c352fb0fd19b
TymoshenkoOl <olenka0701@gmail.com>
2022-12-08 17:47:37 +0200

    Added new variable - mirror_number

--- Mirror_number_25.11.22.py
+++ Mirror_number_25.11.22.py
@@ -2,6 +2,5 @@ three_digit_number = int(input('Введіть тризначне число: ')
 first_digit = three_digit_number // 100
 second_digit = three_digit_number // 10 % 10
 third_digit = three_digit_number % 10
-print(f'{third_digit}{second_digit}{first_digit}')
-
-
+mirror_number = f'{third_digit}{second_digit}{first_digit}'
+print(mirror_number)
