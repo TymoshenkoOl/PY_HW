f58b29d2192de109dfef60a49603934c67ed9b69
TymoshenkoOl <olenka0701@gmail.com>
2022-12-08 19:50:08 +0200

    Added text when python doesn't know what to do

--- Calculator_29.11.22.py
+++ Calculator_29.11.22.py
@@ -10,12 +10,18 @@ second_digit = float(input('Введіть перше число: '))
 action = input('Введіть дію: ')
 if '+' in str(action):
     result = first_digit + second_digit
+    print(result)
 elif '-' in str(action):
     result = first_digit - second_digit
+    print(result)
 elif '*' in str(action):
     result = first_digit * second_digit
+    print(result)
 elif '/' in str(action):
     result = first_digit / second_digit
+    print(result)
 elif '^' in str(action):
     result = first_digit ** second_digit
-print(result)
+    print(result)
+else:
+    print('В наступних версіях буде більше функцій, а поки оберіть дію з нявних у списку')
