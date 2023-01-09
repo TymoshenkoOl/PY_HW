846e567ccd65bd22975ecadf63f96f399f922120
TymoshenkoOl <118847802+TymoshenkoOl@users.noreply.github.com>
2023-01-09 20:07:43 +0000

    Повністю змінено рішення задачі

--- Task3_29.11.22.py
+++ Task3_29.11.22.py
@@ -1,12 +1,19 @@
-any_digit = int(input('Введіть число: '))
-if 11 < any_digit:
-    if any_digit % 2 == 0 or any_digit % 3 == 0 or any_digit % 5 == 0 or any_digit % 7 == 0 or any_digit % 11 == 0:
-        print('Число не є простим')
-    else:
-        print('Число є простим')
-elif 0 < any_digit <= 11:
-    if any_digit % 2 == 0 and any_digit != 2 or any_digit == 1 or any_digit % 3 == 0 and any_digit != 3:
-        print('Число не є простим')
-    else:
-        print('Число є простим')
-
+any_dig = (input('Введіть число відмінне від "0": '))
+try:
+    any_digit = int(any_dig)
+except:
+    print('Не є числом')
+    quit()
+if any_digit == 0 or any_digit == 1:
+    print('Не відноситься ні до простих ні до складених чисел')
+    quit()
+n = 1
+k = 0
+while any_digit >= n:
+    if any_digit % n == 0:
+        k += 1
+    n += 1
+if k <= 2:
+    print('Число є простим')
+else:
+    print('Число є складеним')
