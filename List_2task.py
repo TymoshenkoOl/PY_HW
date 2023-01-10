78d6f82c8071061033408352ec407076c08dc3c3
TymoshenkoOl <118847802+TymoshenkoOl@users.noreply.github.com>
2023-01-10 22:52:05 +0000

    Числа більше 100 в новий список

--- List_2task.py
+++ List_2task.py
@@ -1,7 +1,6 @@
-my_list = [3, 34, 646, 24, 64, 23, 46, 686, 1, 2]
-n = len(my_list)
-if n < 2:
-    my_list.append(0)
-else:
-    my_list.append(my_list[n - 1] + my_list[n - 2])
-print(my_list)
+my_list = [3, 45, 64, 24, 78, 43, 67, 345, 753, 134, 569, 104, 532, 75, 2]
+my_results = []
+for i in my_list:
+    if i >= 100:
+        my_results.append(i)
+print(my_results)
