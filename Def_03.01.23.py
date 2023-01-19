120435fdb86c8b3f6bcbeae5161a48d6189feba6
TymoshenkoOl <118847802+TymoshenkoOl@users.noreply.github.com>
2023-01-19 21:20:00 +0000

    1. Написать функцию которой передается один параметр - список строк my_list. Функция возвращает новый список в котором содержаться элементы из my_list по следующему правилу: Если строка стоит на нечетном месте в my_list, то ее заменить на перевернутую строку. "qwe" на "ewq". Если на четном - оставить без изменения.

---
+++ Def_03.01.23.py
@@ -0,0 +1,16 @@
+def qwerty(my_list):
+    n = 0
+    for i in my_list[n:(len(my_list) + 1)]:
+        if n % 2 == 0:
+            new_list.append(i[::-1])
+            n += 1
+        else:
+            new_list.append(i)
+            n += 1
+    return new_list
+
+
+my_list = input('Enter a list separated by commas: ').split(', ')
+new_list = []
+print(qwerty(my_list))
+print()
